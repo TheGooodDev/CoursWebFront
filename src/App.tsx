7dbbdb0d1b89f0ef01396d326e32951e02a7081e
Felicien <felicien@Felicien-Courdesse>
2023-03-22 10:30:41 +0100

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { useState, useMemo, useEffect } from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
+  
+  useEffect(()=>{
+    fetch("https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port")
+  }) 
 
   return (
     <div className="App">
       <h1>API</h1>
+
     </div>
   )
 }
